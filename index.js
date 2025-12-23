3b79cd158831d0a439af352d339a1b59bc21a073
Morningstar-2279 <ezraking30@gmail.com>
2025-12-23 13:10:45 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers
+    ]
+});
+
+const LOG_CHANNEL_ID = "1452940544006291518";
+
+client.once("ready", () => {
+    console.log(`Bot is logged in as ${client.user.tag}`);
+});
+
+client.on("guildMemberAdd", async (member) => {
+    const logChannel = client.channels.cache.get(LOG_CHANNEL_ID);
+    if (!logChannel) return;
+
+    logChannel.send(
+        `**New Member Joined**\n` +
+        `User: ${member.user.tag} \n` +
+        `Server: ${member.guild.name}`
+    );
+});
+
+client.on("guildMemberRemove", async (member) => {
+  const logChannel = client.channels.cache.get(LOG_CHANNEL_ID);
+  if (!logChannel) return;
+
+  logChannel.send(
+    `**Member Left**\n` +
+    `User: ${member.user.tag}\n` +
+    `Server: ${member.guild.name}`
+  );
+});
+
+client.login("MTQ1MjkzNTI2NjIxMjc3Mzk4OA.GlUOPN.01LVti3B4joHRjbAZjCz8pCeinTvZK2i9kTkVs")
